eb4c3debaf40d1e45cfce4cbf7022fc30c0e1a45
Nil patel <nil.patel@scriptalldna.com>
2024-03-29 16:29:04 +0530

    add model Contact in customer app.

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    precedenceChoices = (("p","primary"),("s","secondary"))
+    phoneNumner = models.CharField(max_length=128, null=True)
+    email = models.CharField(max_length=128, null=True)
+    linkedId = models.IntegerField(null=True)
+    linkPrecedence = models.CharField(max_length=10, choices=precedenceChoices)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    deletedAt = models.DateTimeField(null=True)
