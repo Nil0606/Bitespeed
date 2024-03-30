05b0dd61bf5bef3ff0f6706a825d7d205899eb83
Nil patel <nil.patel@scriptalldna.com>
2024-03-30 17:54:49 +0530

    change settings.py file again

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6#(-34#(u+$*4lw2*sclj=6$*%#(4c*c-*tfy$9#h!z34wkr9)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
